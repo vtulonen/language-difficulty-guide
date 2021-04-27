633f6e2825a480ccbfef9e6fb280bd8647c10500
Vili Tulonen <vili.tulonen@tuni.fi>
2021-04-27 13:30:26 +0300

    rm parantheses with specifying info

--- Languages.h
+++ Languages.h
@@ -12,8 +12,8 @@ struct Languages
     QStringList easiest {"Afrikaans", "Danish", "Dutch", "French", "Italian", "Norwegian", "Portuguese", "Romanian", "Spanish", "Swedish"};
     QStringList easy { "German"};
     QStringList medium { "Indonesian", "Malaysian",	"Swahili"};
-    QStringList hard {     "Albanian",     "Amharic",     "Armenian",     "Azerbaijani",     "Bengali",     "Bosnian",     "Bulgarian",     "Burmese",     "Croatian",     "Czech",     "Estonian",     "Finnish",     "Georgian",     "Greek",     "Hebrew",     "Hindi",     "Hungarian",     "Icelandic",     "Khmer",     "Lao",     "Latvian",     "Lithuanian",     "Macedonian",     "Mongolian",     "Nepali",     "Pashto",     "Persian (Dari",     "Farsi",     "Tajik)",     "Polish",     "Russian",     "Serbian",     "Sinhala",     "Slovak",     "Slovenian",     "Tagalog",     "Thai",     "Turkish",     "Ukrainian",     "Urdu",     "Uzbek",     "Vietnamese",     "Xhosa",     "Zulu" };
-    QStringList hardest { "Arabic", "Cantonese (Chinese)", "Mandarin (Chinese)", "Japanese", "Korean" };
+    QStringList hard {     "Albanian",     "Amharic",     "Armenian",     "Azerbaijani",     "Bengali",     "Bosnian",     "Bulgarian",     "Burmese",     "Croatian",     "Czech",     "Estonian",     "Finnish",     "Georgian",     "Greek",     "Hebrew",     "Hindi",     "Hungarian",     "Icelandic",     "Khmer",     "Lao",     "Latvian",     "Lithuanian",     "Macedonian",     "Mongolian",     "Nepali",     "Pashto",     "Persian",     "Farsi",     "Tajik",     "Polish",     "Russian",     "Serbian",     "Sinhala",     "Slovak",     "Slovenian",     "Tagalog",     "Thai",     "Turkish",     "Ukrainian",     "Urdu",     "Uzbek",     "Vietnamese",     "Xhosa",     "Zulu" };
+    QStringList hardest { "Arabic", "Cantonese", "Mandarin", "Japanese", "Korean" };
     QStringList all = easiest + easy + medium + hard + hardest;
 };
 
